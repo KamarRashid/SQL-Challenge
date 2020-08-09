6b576dbca1cef1eea8ae8b449160a9f9ed4d4a2e
KamarRashid <k.abdur.rashid@mail.utoronto.ca>
2020-08-08 23:09:04 -0400

    add dbd files

--- EmployeeSQL/DB_Diagram/DBD-export_code.sql
+++ EmployeeSQL/DB_Diagram/DBD-export_code.sql
@@ -23,11 +23,11 @@ CREATE TABLE "dept_manager" (
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
-    "lasyt_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
